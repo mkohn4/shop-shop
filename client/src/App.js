adafb28b12b82190c9d91065580abd745825b577
Max Kohn <mkohn@mkohn-ltm4.internal.salesforce.com>
2022-03-01 10:19:11 -0500

    feature/global-store

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,8 @@ import Signup from './pages/Signup';
 import Nav from './components/Nav';
 import OrderHistory from './pages/OrderHistory';
 
+import { StoreProvider } from './utils/GlobalState';
+
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
@@ -40,6 +42,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
+          <StoreProvider>
           <Nav />
           <Switch>
             <Route exact path="/" component={Home} />
@@ -49,6 +52,7 @@ function App() {
             <Route exact path="/products/:id" component={Detail} />
             <Route component={NoMatch} />
           </Switch>
+          </StoreProvider>
         </div>
       </Router>
     </ApolloProvider>
--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../../utils/queries';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
+import {useStoreContext} from '../../utils/GlobalState';
 
-function CategoryMenu({ setCategory }) {
+function CategoryMenu() {
+
+  //get current state from global state
+  const [state, dispatch] = useStoreContext();
+  //destructure categories out of global state because its all we need here
+  const {categories} = state;
+  //get category data by querying db with QUERY_CATEGORIES
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  const categories = categoryData?.categories || [];
+
+  //when component loads and categoryData is now defined, dispatch sets categoryData to global state
+  //function runs on load to update global state
+  //when useQuery finishes and data exists in categoryData, useEffect runs again and notices categoryData exists
+  //then it executes dispatch() function
+  useEffect(() => {
+    //if categoryData exists or has changed from the response of useQuery, then run dispatch()
+    if (categoryData) {
+      //execute our dispatch function with our action object to indicate
+      //type of action and data to set our state for categories to
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories
+      });
+    }
+  },[categoryData, dispatch])
+
+  //update click handler to update global state
+  const handleClick = id => {
+    dispatch({
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id
+    });
+  };
+  
+  //const categories = categoryData?.categories || [];
 
   return (
     <div>
@@ -13,7 +46,7 @@ function CategoryMenu({ setCategory }) {
         <button
           key={item._id}
           onClick={() => {
-            setCategory(item._id);
+            handleClick(item._id);
           }}
         >
           {item.name}
